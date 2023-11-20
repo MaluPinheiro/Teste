b1189a7a9825a6911dc962d84362b1b158d8ed41
Maria Luiza Pinheiro <102831221+MaluPinheiro@users.noreply.github.com>
2023-11-20 08:57:28 -0300

    Update UsuarioServices.ts

--- src/services/UsuarioServices.ts
+++ src/services/UsuarioServices.ts
@@ -1,23 +1,73 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-class UsuarioServices{
-    constructor(){}
+class UsuarioService {
+    constructor(){
 
-    async ListarUsuario(){
-        try {
-            const usuario = await prisma.usuario.findMany()
-            return usuario
+    }
+
+    async listUsers(id?: int){
+        try{
+            if(id){
+                const user = await prisma.usuario.findUnique({
+                    where: {
+                        id
+                    }
+                });
+                return user;
+            }else{
+                const users = await prisma.usuario.findMany();
+                return users;
+            }
         }catch(error){
-            console.log(error)
+            console.log(error);
+            return null;
         }
     }
 
-    async criarUsuario(){}
+    async createUser(user: Prisma.UsuarioCreateInput){
+        try{
+            const newuser = await prisma.usuario.create({
+                data: user
+            });
+            return newuser;
+        }catch(error){
+            console.log(error);
+            return null;
+        }
+    }
 
-    async atualizarUsuario(){}
+    async updateUser(id: string, user: Prisma.UsuarioUpdateInput){
+        try{
+            const updatedUser = await prisma.usuario.update({
+                where: {
+                    id
+                },
+                data: user
+            });
 
-    async deletarUsuario(){}
+            return updatedUser;
+        }catch(error){
+            console.log(error);
+            return null;
+        }
+    }
+
+    async deleteUser(id: string){
+        try{
+            const deletedUser = await prisma.usuario.delete({
+                where: {
+                    id
+                }
+            });
+
+            return deletedUser;
+        }catch(error){
+            console.log(error);
+            return null;
+        }
+    }
 }
-export default new UsuarioServices
+
+export default new UsuarioService();
